4015b9a0fc6df2e37446917890da8c9654121de5
Sagar <dev.sagartmg@gmail.com>
2024-05-19 17:08:04 +0545

    js variables

---
+++ js/index.js
@@ -0,0 +1,34 @@
+/* DRY : donot repeat yourself   */
+
+greeting = "Namaste";
+
+console.log("in greeting variable, we have value", greeting);
+
+/* 
+    alt + shift + a for multi line comments
+    
+    console.log("Namaste", "hari");
+    console.log("Namaste", "sita");
+    console.log("Namaste", "gita");
+    console.log("Namaste", "shyam");
+ */
+
+console.log(greeting, "hari");
+console.log(greeting, "sita");
+console.log(greeting, "gita");
+console.log(greeting, "shyam");
+console.log(greeting, "abc");
+console.log(greeting, "ijk");
+console.log(greeting, "xyz");
+
+var color = "red";
+let age = 10;
+const isActive = true;
+
+console.log("age", age);
+console.log("isActive", isActive);
+
+
+/* research
+    var vs let vs  const
+*/
