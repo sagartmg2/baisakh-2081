35ae379649ae45d76eb09ad728d52974b39c35b9
Sagar <dev.sagartmg@gmail.com>
2024-05-20 17:08:51 +0545

    data types

---
+++ js/2-let-vs-const.js
@@ -0,0 +1,125 @@
+console.log("js connected")
+
+/* variable 
+    simpy a keyword which can store some values. 
+*/
+
+greeting = "hello"
+
+console.log("greeting", greeting)
+
+console.log(greeting, "ram")
+console.log(greeting, "sita")
+console.log(greeting, "hari")
+
+age = 10
+// person name = "ram" // wrong
+// personname = "ram" //  wrong // difficult to read.
+
+person_name = "ram" //  snake_case
+personName = "ram" // camelCase
+/* we can use any. just maintain consistency. */
+
+// 1position = "static"
+position1 = "static"
+position2 = "realtive"
+
+console.log(person_name, "age is ", age)
+console.log(personName, "age is ", age)
+
+fullName = "hari"
+grade = 10
+isActive = false
+
+fullName = "hari bdr"
+isActive = true
+
+console.log(fullName)
+console.log(isActive)
+
+/* let vs const 
+    
+    declartion vs initialization
+
+*/
+
+var shape = "circle"
+var shape = "square"
+var shape = "oval"
+
+let color = "red"
+console.log(color)
+
+//  let color = "red" // cannot redclare variable already created using let keyword
+color = "white"
+color = "black"
+
+let size // declartion
+size = "sm" // initialzation
+size = "md" // re-initalization
+size = "lg" // re-initalization
+
+console.log(size)
+
+let price
+price = 1000
+price = 10000
+
+const PI = 3.14
+console.log(PI)
+
+const ROLE = "customer" // suppose fetched from database
+// ROLE = "admin"
+
+let apiData = ""
+apiData = "products data"
+
+console.log(apiData)
+
+/* Data types */
+let userName = "ram"
+userName = "ram"
+userName = `ram`
+let description =
+  "orem ipsum dolor sit amet consectetur adipisicing elit. Eligendi illo vitae laboriosam magnam dolorem, nam accusamus similique sapiente ab numquam consequuntur nihil possimus ducimus consequatur quos voluptatemesse qui obcaec"
+let userAge = 10
+let userActiveStatus = true
+userActiveStatus = false
+
+/* 
+    1. text  - string
+    2. number  - number
+        - whole number
+        - decimal / float / double
+    3. true/false - boolean
+
+    4. undefined // in js only
+    5. null // empty values
+
+
+    6. array
+    7. object
+
+*/
+
+let count
+console.log("count", count)
+
+let userData = null
+
+/* on button click , fetch users data 
+    userData = "user data"
+*/
+
+
+let pricePrice = 100
+pricePrice = "hundared fifty"
+
+
+/* 
+    research  on 
+    array 
+*/
+
+
+
