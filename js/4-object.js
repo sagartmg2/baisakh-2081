c6cd81ca087eda20843fdde60a27a6ec5d5b650b
Sagar <dev.sagartmg@gmail.com>
2024-05-24 17:09:02 +0545

    array and objects

--- js/4-object.js
+++ js/4-object.js
@@ -11,10 +11,8 @@ let user3 = {
   age: 20,
 }
 
-
 let users = [user1, user2, user3]
 console.log(users) //we need age, contact of every users
-
 
 let usersData = [
   {
@@ -23,7 +21,7 @@ let usersData = [
   },
   {
     name: "shyam",
-    age: -15,  // change this to 15
+    age: -15, // change this to 15
   },
   {
     name: "hari",
@@ -32,28 +30,48 @@ let usersData = [
 ]
 
 
+
+
+
 /* code here  */
 //  change this to 15
-console.log(usersData);
-
-
+console.log(usersData)
 
 /* research on : nested objects */
 let person = {
-    name:"hari",
-    age:12,
-    contact:12321343,
+  name: "hari",
+  age: 12,
+  contact: 12321343,
 
-    /*  address , permamenet address: district,province,wrd  , temporarary address : district,province,wrd  */
-
+  /*  address , permamenet address: district,province,wrd  , temporarary address : district,province,wrd  */
 }
 
-console.log(person);
-
+console.log(person)
 
 // console.log(friends);
 // console.log(courses);
 // console.log(products);
 
+let courseTitle = "mern"
+let courseDuration = "3 months"
+let courseTopics = ["html", "Css", "js"]
+let courseTiming = "4-5"
+
+let course = {
+  title: "mern",
+  duration: "3 months",
+  topics: ["html", "Css", "js"],
+  timing: "4 - 5 ",
+}
 
 
+
+let person1 = {
+    name: "Hari",
+    age: 22,
+    contact: 9866377067,
+    address: {
+      permandentAddress:"jhapa",
+      temporaryaddress:"ktm"
+    }
+  }
---
+++ js/5-object-array.js
@@ -0,0 +1,92 @@
+let usersData = [
+  {
+    name: "ram",
+    age: 10,
+  },
+  {
+    name: "shyam",
+    age: -15, // change this to 15
+  },
+  {
+    name: "hari",
+    age: 20,
+  },
+]
+
+console.log(usersData[1].age)
+usersData[1].age = 15
+console.log(usersData)
+
+let users = ["ram", "shy", "hari"]
+console.log(users[1])
+users[1] = "shyam"
+
+console.log(users)
+
+// let person  = "hari"
+// MindRisers!@
+
+let course = {
+  title: "mern",
+  duration: "2 months",
+  topics: ["html", "Css", "js"],
+  timing: "4 - 5 ",
+}
+
+let person = {
+  name: "hari",
+  age: 12,
+  contact: 12321343,
+  address: {
+    permananetAddress: {
+      province: "1",
+      district: "jhapa",
+      //   houseNumber:12, // add through code
+      tole: "abc",
+      municipality: {
+        name: "unknown", // jhapa municipality  person.address.permananetAddress.municipality= "jhapa"
+        ward: 5,
+      },
+    },
+    temporaryAddress: {
+      province: "3",
+      district: "kathamdnu",
+      tole: "balaju",
+      municipality: {
+        name: "kathmandu nagarpalika",
+        ward: 15,
+      },
+    },
+  },
+}
+
+let students = [
+  {
+    name: "ram",
+    courses: [
+      {
+        title: "web",
+      },
+      {
+        title: "ui - ux ",
+      },
+    ],
+  },
+  {
+    name: "hari",
+    courses: [{ title: "mern" }],
+  },
+  {
+    name: "sita",
+    courses: [
+      {
+        title: "web",
+      },
+      {
+        title: "ux", // changet this to ui ux
+      },
+    ],
+  },
+]
+
+let classes = ["one", "two", "there"] // sections ABC , classteacher, students
