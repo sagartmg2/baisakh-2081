80c2c71964bb4bd8d6fe13441a50615eefd53e4e
Sagar <dev.sagartmg@gmail.com>
2024-07-09 17:06:45 +0545

    configure tailwid

--- ecommerce-client/src/App.jsx
+++ ecommerce-client/src/App.jsx
@@ -1,5 +1,18 @@
+import { CiMail } from "react-icons/ci"
+import { FaPhoneAlt } from "react-icons/fa"
+
 function App() {
-    return <>app component</>
+    return (
+        <>
+            <header className="bg-[#7E33E0] text-white p-4 flex justify-between">
+                <div className="flex ">
+                    <CiMail /> emai@gmaicom
+                    <FaPhoneAlt /> 987325345234
+                </div>
+                <div>login loginicon cart</div>
+            </header>
+        </>
+    )
 }
 
 export default App
